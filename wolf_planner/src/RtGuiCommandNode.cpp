6b16282150489bfbe3045d377f280c09a7d65546
Gennaro Raiola <gennaro.raiola@gmail.com>
2025-05-20 17:00:07 +0200

    Add RtGuiGaitCommandNode

---
+++ wolf_planner/src/RtGuiCommandNode.cpp
@@ -0,0 +1,22 @@
+#include "wolf_planner/RtGuiGaitPublisher.h"
+
+int main(int argc, char* argv[]) {
+  ros::init(argc, argv, "wolf_planner_rt_gui_node");
+  ros::NodeHandle nh;
+
+  // Get node parameters
+  std::string gaitCommandFile;
+  const std::string topicPrefix = "wolf_planner";
+  nh.getParam(topicPrefix+"/gaitCommandFile", gaitCommandFile);
+  std::cerr << "Loading gait file: " << gaitCommandFile << std::endl;
+
+  try {
+    wolf::RtGuiGaitPublisher guiNode(nh, gaitCommandFile, topicPrefix);
+    guiNode.run();
+  } catch (const std::exception& e) {
+    ROS_FATAL_STREAM("Exception caught: " << e.what());
+    return 1;
+  }
+
+  return 0;
+}
